2f05498defba60ae948ea001fc2282e11fa52d30
CaiyueDamowang <www.caiyue@qq.com>
2020-03-12 23:26:49 +0800

    解析url

--- urlStringfy.js
+++ urlStringfy.js
@@ -5,13 +5,14 @@ function queryStringfy(s) {
         const [key,value] = item.split('=')
         if(querys[key]){
             Array.isArray(querys[key]) && querys[key].push(value)
-            !Array.isArray(querys[key]) && (querys[key] = [querys[key]]) && querys[key].push(value)
+            !Array.isArray(querys[key]) && (querys[key] = [querys[key],value])
         }else{
             querys[key] = value
         }
     })
     return querys
 }
+
 console.log(
     queryStringfy('s=1&s=2&q=j&w=w')
 )
