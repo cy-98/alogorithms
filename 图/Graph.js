91be1f34f997fbce9bbea3cfe6b7e066846188c1
CaiyueDamowang <www.caiyue@qq.com>
2020-06-19 18:53:21 +0800

    nodelist

--- 图/Graph.js
+++ 图/Graph.js
@@ -2,11 +2,16 @@ function Graph(v) {
     this.vertices = v
     this.adj = []
     this.edges = 0
+    this.marked = []
+    for(let i = 0; i < this.vertices; i++) {
+        this.adj[i] = []
+        this.marked[i] = false
+    }
 }
 
 Graph.prototype = {
     showGraph() {
-        for(let i = 0; i < this.vertices.length; i++) {
+        for(let i = 0; i < this.vertices; i++) {
             let path = `${i} -> `
             for(let j = 0; j < this.adj[i].length; j++) {
                 path += `${this.adj[i][j]}`
@@ -16,7 +21,43 @@ Graph.prototype = {
     },
     addEdge(v, w) {
         this.adj[v].push(w)
-        this.ad[w].push(v)
+        this.adj[w].push(v)
         this.edges ++
+    },
+    // 深度优先搜索
+    dfs(v) {
+        this.marked[v] = true
+        console.log(`visited ${v}`)
+        for(let i = 0; i < this.adj[v].length; i++) {
+            const w = this.adj[v][i]
+            if(!this.marked[w]) { 
+                this.dfs(this.adj[v][i])
+            }
+        }
+    },
+    // 宽度优先搜索
+    bfs(v) {
+        let queue = []
+        queue.push(v)
+        this.marked[v] = true
+        while(queue.length) {
+            const v = queue.shift()
+            for(let i = 0; i < this.adj[v].length; i++) {
+                const w = this.adj[v][i]
+                if(!this.marked[w]) {
+                    this.marked[w] = true
+                    queue.push(w)
+                    console.log(w)
+                }
+            }
+        }
     }
 }
+
+const g = new Graph(5)
+g.addEdge(0,1)
+g.addEdge(0,2)
+g.addEdge(1,3)
+g.addEdge(2,4)
+g.showGraph()
+g.bfs(0)
